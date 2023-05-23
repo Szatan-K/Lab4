bc0e4dfec490c44f7279804bb41fb5ae0c8ecdd4
Szatan-K <kacperszatan03@gmail.com>
2023-05-23 20:30:23 +0200

    czwarty Branch

--- skrypt.sh
+++ skrypt.sh
@@ -3,6 +3,18 @@
 if [[ $1 == "--date" ]]; then
     dzisiejsza_data=$(date +%Y-%m-%d)
     echo "Dzisiejsza data to: $dzisiejsza_data"
+elif [[ $1 == "--logs" ]]; then
+    if [[ -n $2 ]] && [[ $2 =~ ^[0-9]+$ ]]; then
+        i=$2
+    else 
+        i=100
+    fi
+    for ((x=1; x<=$i; x++)); do
+    nazwa_pliku="log$x.txt"
+    echo "Nazwa pliku: $nazwa_pliku" >> "$nazwa_pliku"
+    echo "Stworzony przez: $0" >> "$nazwa_pliku"
+    echo "Data stworzenia: $(date)" >> "$nazwa_pliku"
+    done
 else
-    echo "Niepoprawna opcja"
+    echo "Niepoprawna opcja."
 fi
