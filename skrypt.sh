0b6ab3dc6f62f0b6216af724893faa078a532da3
Szatan-K <kacperszatan03@gmail.com>
2023-05-23 20:20:01 +0200

    trzeci Branch

--- skrypt.sh
+++ skrypt.sh
@@ -4,5 +4,5 @@ if [[ $1 == "--date" ]]; then
     dzisiejsza_data=$(date +%Y-%m-%d)
     echo "Dzisiejsza data to: $dzisiejsza_data"
 else
-    echo "Niepoprawna opcja."
+    echo "Niepoprawna opcja"
 fi
