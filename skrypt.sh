0f887592d795b4ceba4dd509f2132fa9eaf5801e
Szatan-K <kacperszatan03@gmail.com>
2023-05-23 21:07:32 +0200

    szosty Branch

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $1 == "--date" ]]; then
+if [[ $1 == "--date" ]] || [[ $1 == "-d" ]]; then
     dzisiejsza_data=$(date +%Y-%m-%d)
     echo "Dzisiejsza data to: $dzisiejsza_data"
-elif [[ $1 == "--logs" ]]; then
+elif [[ $1 == "--logs" ]] || [[ $1 == "-l" ]]; then
     if [[ -n $2 ]] && [[ $2 =~ ^[0-9]+$ ]]; then
         i=$2
     else 
@@ -15,11 +15,11 @@ elif [[ $1 == "--logs" ]]; then
     echo "Stworzony przez: $0" >> "$nazwa_pliku"
     echo "Data stworzenia: $(date)" >> "$nazwa_pliku"
     done
-elif [[ $1 == "--help" ]]; then
+elif [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
     echo "Dostepne opcje:"
-    echo "--date - wyswietlenie dzisiejszej daty."
-    echo "--logs - tworzenie plikow tekstowych (istnieje mozliwosc wybrania liczby plikow do stworzenia. Domyslnie 100)"
-    echo "--help - Wyswietlenie dostepnych opcji."
+    echo "--date, -d - wyswietlenie dzisiejszej daty."
+    echo "--logs, -l - tworzenie plikow tekstowych (istnieje mozliwosc wybrania liczby plikow do stworzenia. Domyslnie 100)"
+    echo "--help, -h - wyswietlenie dostepnych opcji."
 else
     echo "Niepoprawna opcja."
 fi
